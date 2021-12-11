51d7986659c2755e41c52016c7816f5d4df7ae23
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2021-12-11 18:22:01 +0300

    porobably fixed padding issue

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Footer from "./footer";
 import Header from "./header";
+import Head from "next/head";
 
 const Layout = (props) => {
   useEffect(() => {
@@ -9,12 +10,15 @@ const Layout = (props) => {
 
   return (
     <div className="w-screen h-screen">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Header {...props.children.props.header} />
-      <main className="w-full lg:w-screen">
+      <div className="w-full lg:w-screen">
         {React.cloneElement(props.children, {
           notification_props: props.notification_props,
         })}
-      </main>
+      </div>
       <Footer />
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import "tailwindcss/tailwind.css";
 import "semantic-ui-css/semantic.min.css";
 import "../styles/global.css";
-import "../styles/skeleton-loading.css";
-import { useEffect } from "react";
 import { Provider as NotificationProvider } from "../hooks/useNotification";
-import Header from "../components/header";
 import Layout from "../components/layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
